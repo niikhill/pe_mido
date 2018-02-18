467a67832277c4057e886b4f32ae2dc2777312ab
Nikhil <5293647+niikhill@users.noreply.github.com>
2018-02-18 17:10:34 +0000

    doze

--- device.mk
+++ device.mk
@@ -365,8 +365,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/vendor/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat 
+
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
