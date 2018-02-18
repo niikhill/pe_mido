61c9653481c916701a7be4e9cc1348c12728a563
Nikhil <5293647+niikhill@users.noreply.github.com>
2018-02-18 16:58:22 +0000

    Revert "mido: add Ambient display (XiaomiDoze)"
    This reverts commit 61ad9e61bc0c6665d6d5bb383826ce74f97da694.

--- device.mk
+++ device.mk
@@ -160,10 +160,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
